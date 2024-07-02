a84d7161e98b900b942c1ae76fec80ac76cbda56
khaledssbd <khaledssbd@gmail.com>
2024-07-02 17:00:40 +0600

    add readme 4

--- index.js
+++ index.js
@@ -541,8 +541,6 @@ async function run() {
       });
     });
 
-
-
     // Send a ping to confirm a successful connection to DB
     await client.db('admin').command({ ping: 1 });
     console.log(
